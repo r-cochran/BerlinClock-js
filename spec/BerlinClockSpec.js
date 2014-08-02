14e0a316973e7b1d906d90200417f2b5434adee3
Robert Cochran <cochrarj@muohio.edu>
2014-08-01 22:14:34 -0400

    added dom affix to spec setup

--- spec/BerlinClockSpec.js
+++ spec/BerlinClockSpec.js
@@ -2,10 +2,14 @@ describe("BerlinClock", function(){
 	var berlinClock;
 	beforeEach(function(){
 		berlinClock = new BerlinClock();
-		affix("div#berlinClock");
+		affix("div#seconds div");
+		affix("div#hoursOfFive div+div+div+div");
+		affix("div#hoursOfOne div+div+div+div");
+		affix("div#minutesOfFive div+div+div+div+div+div+div+div+div+div+div");
+		affix("div#minutesOfOne div+div+div+div");
 	});
 
 	it("does more stuff", function(){
-		
+
 	});
 });
